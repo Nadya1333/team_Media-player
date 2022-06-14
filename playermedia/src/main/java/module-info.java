c5af96d8e656ef5ab929559c2169009768a75c46
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2022-06-14 19:49:09 +0300

    Delete playermedia directory

--- playermedia/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.playermedia {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.playermedia to javafx.fxml;
-    exports com.example.playermedia;
-}
