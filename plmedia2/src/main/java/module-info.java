977a7324c8aa7064489853304e8c8b3544c58d9f
DaryaFilimonova7 <91047620+DaryaFilimonova7@users.noreply.github.com>
2022-06-14 19:40:24 +0300

    Delete plmedia2 directory

--- plmedia2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.playermedia {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.example.playermedia to javafx.fxml;
-    exports com.example.playermedia;
-}
